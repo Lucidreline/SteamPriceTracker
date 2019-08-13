c3ffe15b17d8a5243953d42f65b55d1973e0a4c8
Manuel Castaneda <Ma52Castaneda@gmail.com>
2019-08-12 23:02:42 -0700

    I can now post the owner and link on a spreadsheet sheet and have the app read that and then evenly add it on the next sheet. Also I added a lot of comments

--- app.py
+++ app.py
@@ -1,114 +1,150 @@
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-from selenium import webdriver
-from datetime import datetime
+from   oauth2client.service_account import ServiceAccountCredentials
+from   selenium import webdriver
+from   datetime import datetime
 
+#this opens up a chrome window
 browser = webdriver.Chrome()
+#this minimizes the window so that I dont have to see the amazon page
 browser.minimize_window()
 
-#Spreadsheet config
+#Spreadsheet config - - -
+#the name of the google spreadsheet
 SPREADSHEET_NAME = "AmazonPriceChecker"
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+#honestly no clue what this does, I just know it is needed
+scope  = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+#goes thru my credentials. I got this from the google API credentials
+creds  = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 client = gspread.authorize(creds)
-sheet = client.open(SPREADSHEET_NAME).sheet1
+# Lets me choose if I want to write/read different sheets.
+sheet1 = client.open(SPREADSHEET_NAME).get_worksheet(0)
+sheet2 = client.open(SPREADSHEET_NAME).get_worksheet(1)
 
-class item:
-    def __init__(self, link, owner):
-        self.link = link
-        self.name = ""
-        self.price = 0
-        self.owner = owner
-        self.num = 0 #this determins where on the spreadsheet the data will go
-        self.needsToPrintInitialValues = True
+#creates an empty list where all the item objects will be stored
+itemsList = []
+
+
+class Item:
+    def __init__(self, owner, link):
+        self.link  = link  #link to the amazon page where the price and name are found
+        self.name  = ""    #This is assigned when the amazon page is pulled up
+        self.price = 0     #This is assigned when the amazon page is pulled up
+        self.owner = owner #who is tracking the product
+        self.num   = 0     #this determins where on the spreadsheet the data will go
+        self.needsToPrintInitialValues = True #This is assigned later
 
 def GetDate():
+    #gets the current time
     currentTime = datetime.now()
 
+    #gets the month & day and makes sure that it is 2 digits atleast; 5 -> 05
     month = f"{currentTime.month:02d}"
-    day = f"{currentTime.day:02d}"
-    year = str(currentTime.year)[-2:]
+    day   = f"{currentTime.day:02d}"
+    #gets last 2 digits of the year
+    year  = str(currentTime.year)[-2:] 
 
+    #puts date into 'mm/dd/yy' format
     return str(month + "/" + day + "/" + year)
 
-def getInfo(_item):
+def ReadUserList(_itemsList):
+    #loop each row until an empty row is found
+    for rowNum in range(10000):
+        #adds 3 because I want it to start looking on row 3
+        rowNum = rowNum + 3
+        
+        #checks to see if the cell contains data; if its not empty
+        if(sheet1.cell(rowNum, 1).value != ""):
+            #pack info from spreadsheet into an object
+            itemObject = Item(sheet1.cell(rowNum, 1).value, sheet1.cell(rowNum, 2).value)
+            #send object to the front of the list
+            _itemsList.append(itemObject)
+        else:
+            #reverses the order of the items list. This is because I was only able to append objects
+            #which put new objects in the front of the list. I want new objects at the end of the list.
+            _itemsList.reverse()
+            #breaks out of the rowNum loop
+            break
+                   
+def GetProductInfo(_itemsList):
     #takes in a list of objects
 
-    numOfItems = len(_item)
+    #gets the number of objects in the list of objects
+    numOfItems = len(_itemsList)
 
+    #Loops through every object in the list
     for i in range(numOfItems):
-        _item[i].num = i
+        #assigns each element a number to determine where on the spreadsheet this will be written
+        _itemsList[i].num = i
 
-        browser.get(_item[i].link)
+        #opens the link to the amazon product
+        browser.get(_itemsList[i].link)
         
         try:
-            _item[i].price = browser.find_element_by_id("priceblock_ourprice").text
+            #tries to find the price text. It could be under the 'ourprice' ID
+            _itemsList[i].price = browser.find_element_by_id("priceblock_ourprice").text
         except:
-            _item[i].price = browser.find_element_by_id("priceblock_dealprice").text
+            #this is ran if the 'ourprice' ID was not found. That means that the product is on a deal
+            #It looks for the dealprice instead
+            _itemsList[i].price = browser.find_element_by_id("priceblock_dealprice").text
 
-        _item[i].name = browser.find_element_by_id("productTitle").text
+        #finds the name of the product
+        _itemsList[i].name = browser.find_element_by_id("productTitle").text
+
+        #If it is the last object in the list, then it closes the browser
         if(i == (numOfItems - 1)):
-            print("Close browser. i = " + str(i) + " and num of items = " + str(numOfItems))
             browser.close()
         
-def updateSpreadSheet(_items):
-    for i in range(len(_items)):
-        startingColumn = (6 * _items[i].num) + 1
+def UpdateSpreadSheet(_itemsList):
+    #loops through every object in the objects list
+    for i in range(len(_itemsList)):
+        #uses the assigned object number to determine where the first column of the data will be
+        startingColumn = (6 * _itemsList[i].num) + 1
 
-        #check to see if the name, link and owner are already on row 3(first)
+        #checks to see if the name, link and owner are already on row 3(first row)
+        #5 for range because each item takes up 5 columns
         for j in range(5):
-            if(sheet.cell(3, startingColumn + j).value != ""):
-                _items[i].needsToPrintInitialValues = False
+            #if all cells in the item's cells are empty
+            if(sheet2.cell(3, startingColumn + j).value != ""):
+                _itemsList[i].needsToPrintInitialValues = False
         
-        if(_items[i].needsToPrintInitialValues == True):
+        #this prints out the initial information
+        if(_itemsList[i].needsToPrintInitialValues == True):
+            #starting column is determined by the item number, then I add a number to that
+            #to put information in the columns next to it
+
             #name, link, and owner have not been written.
-            sheet.update_cell(2, startingColumn + 0, "Owner")
-            sheet.update_cell(2, startingColumn + 1, "Product Name")
-            sheet.update_cell(2, startingColumn + 2, "Product Link")
-            sheet.update_cell(2, startingColumn + 3, "Date")        
-            sheet.update_cell(2, startingColumn + 4, "Product Price")
+            sheet2.update_cell(2, startingColumn + 0, "Owner")
+            sheet2.update_cell(2, startingColumn + 1, "Product Name")
+            sheet2.update_cell(2, startingColumn + 2, "Product Link")
+            sheet2.update_cell(2, startingColumn + 3, "Date")        
+            sheet2.update_cell(2, startingColumn + 4, "Product Price")
 
-
-            sheet.update_cell(3, startingColumn + 0, _items[i].owner)
-            sheet.update_cell(3, startingColumn + 1, _items[i].name[:50])
-            sheet.update_cell(3, startingColumn + 2, _items[i].link)
-            sheet.update_cell(3, startingColumn + 3, GetDate())        
-            sheet.update_cell(3, startingColumn + 4, _items[i].price) 
+            #Puts the first day's information
+            #thi is printed a row under ^
+            sheet2.update_cell(3, startingColumn + 0, _itemsList[i].owner)
+            sheet2.update_cell(3, startingColumn + 1, _itemsList[i].name[:50])
+            sheet2.update_cell(3, startingColumn + 2, _itemsList[i].link)
+            sheet2.update_cell(3, startingColumn + 3, GetDate())        
+            sheet2.update_cell(3, startingColumn + 4, _itemsList[i].price) 
             
             
         else:
             #look for next open row
             for rowCheck in range(10000):
-                #rowCheck + 4 because I want it to start looking on row 4; rowCheck will start at 0
-                if(sheet.cell((rowCheck + 4), startingColumn).value == ""):
-                    firstOpenRow = rowCheck + 4
-                    sheet.update_cell(firstOpenRow, startingColumn + 3, GetDate())
-                    sheet.update_cell(firstOpenRow, startingColumn + 4, _items[i].price)
+                #rowCheck + 3 because I want it to start looking on row 3; rowCheck will naturally start at 0
+                if(sheet2.cell((rowCheck + 3), startingColumn + 3).value == ""):
+                    firstOpenRow = rowCheck + 3
+                    #prints out the date and price on the next open row same column
+                    sheet2.update_cell(firstOpenRow, startingColumn + 3, GetDate())
+                    sheet2.update_cell(firstOpenRow, startingColumn + 4, _itemsList[i].price)
                     break
 
-            
 
-            
-            
-            
-            
-        
-            
-        
-        #find the next open row
-        
-        #write in the next open row only what should be repeated
+#Run the 3 main methods and pass in the list of item objects
+ReadUserList(itemsList)
+GetProductInfo(itemsList)
+UpdateSpreadSheet(itemsList)
 
-        
 
-surfacePro6 = item("https://www.amazon.com/gp/product/B07KWNFC64?pf_rd_p=183f5289-9dc0-416f-942e-e8f213ef368b&pf_rd_r=FT3AKVF30EJ4XZYZBHJW", "manny")
-gamingLaptop = item("https://www.amazon.com/gp/slredirect/picassoRedirect.html/ref=sspa_dk_detail_6?ie=UTF8&adId=A0863551GR69NSZW1EPG&qualifier=1565551617&id=485013166934304&widgetName=sp_detail2&url=%2Fdp%2FB07T9KMB91%2Fref%3Dsspa_dk_detail_6%3Fpsc%3D1", "Adriana")
-ipadPro = item("https://www.amazon.com/Apple-10-5-inch-Wi-Fi-Cellular-512GB/dp/B0756651KN?ref_=Oct_TopRatedC_1232597011_0&pf_rd_r=WE348994WA2CQ1M0NXWE&pf_rd_p=f95c3cb9-0b52-52f9-b410-6f99e416af5a&pf_rd_s=merchandised-search-10&pf_rd_t=101&pf_rd_i=1232597011&pf_rd_m=ATVPDKIKX0DER", "manny")
-pen = item("https://www.amazon.com/gp/slredirect/picassoRedirect.html/ref=sspa_dk_detail_1?ie=UTF8&adId=A00650512D7DORHT8DVS4&qualifier=1565551626&id=3061029219346475&widgetName=sp_detail&url=%2Fdp%2FB07C2TFPR2%2Fref%3Dsspa_dk_detail_1%3Fpsc%3D1%26pd_rd_i%3DB07C2TFPR2%26pd_rd_w%3DHVX2Y%26pf_rd_p%3D8a8f3917-7900-4ce8-ad90-adf0d53c0985%26pd_rd_wg%3DvzYee%26pf_rd_r%3DJE3YHK59DJHXA1FXQV8G%26pd_rd_r%3De3c2ba66-eb95-45da-846e-b100e7b0b04a", "manny")
-
-items = [surfacePro6, gamingLaptop, ipadPro, pen]
-
-getInfo(items)
-updateSpreadSheet(items)
 
 
